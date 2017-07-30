fd7eb7829ecf37c905f20401850c5be0f6e38214
Daniel Harmsworth <atrophy@caffeinator.net>
2017-07-30 16:29:48 +0800

    Started work on base logic

---
+++ helmposition-n2k-firmware/helm-position-n2k-firmware.ino
@@ -0,0 +1,19 @@
+#define SENSOR_PIN A1           // Pin connected to the position sensor
+#define SENSOR_RANGE 3141       // Full sensor range in milliradians
+
+void setup() {
+  pinMode(SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  
+
+}
+
+float getHelmPos() {
+  int helmPosRaw = analogRead(SENSOR_PIN);
+  
+  // Dead ahead is 0 radians turn, negative number is turning to port, positive to starboard.
+  int helmPos = map(helmPosRaw, 0, 1024, -SENSOR_RANGE/2, SENSOR_RANGE/2);
+}
+
